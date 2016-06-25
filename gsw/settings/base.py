cbf9853726ac55d78f0ccc8e46f41c25653264f4
¨frendo <geewatt@gmail.com>
2016-06-25 19:42:33 +0100

    Update base.py

--- gsw/settings/base.py
+++ gsw/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '07y6%=qea-70!@ayws-&2yj^5f1a6$93isf80az2-tq3@^bmq^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
