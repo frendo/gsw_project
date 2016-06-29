5518c28892e4cd21dc1d49760bebcfd90ea16a6e
¨frendo <geewatt@gmail.com>
2016-06-29 16:55:16 +0100

    POSTGRE update

--- gsw/settings/production.py
+++ gsw/settings/production.py
@@ -4,5 +4,18 @@ DEBUG = False
 psycopg2==2.6.1
 python-social-auth==0.2.19
 six==1.10.0
-
+DATABASES = { 
+         'default': {
+                     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                     'NAME': get_env_variable('DATABASE_NAME'),
+                     'USER': get_env_variable('DATABASE_USER'),
+                     'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+                     'HOST': 'frendo-165.postgres.pythonanywhere-services.com',
+                     'PORT': '10165',                                                                                                                                                                         
+                 }   
+ }
+ 
+FIXTURE_DIRS = ( 
+    os.path.join(BASE_DIR, 'fixtures'),
+)
 
