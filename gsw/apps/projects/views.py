8cda0b2d21411058689b573f66d07008e106cc93
¨frendo <geewatt@gmail.com>
2016-07-03 16:35:36 +0100

    Adding additional code for two apps

--- gsw/apps/projects/views.py
+++ gsw/apps/projects/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Project
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 def project_list(request):
-    return render(request, 'projects/project_list.html', {})
+    projects = Project.objects.filter(completed_date__lte=timezone.now()).order_by('completed_date')
+    return render(request, 'projects/project_list.html', {'projects' : projects})
+
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, 'projects/project_detail.html', {'project': project})
